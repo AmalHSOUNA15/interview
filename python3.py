4710c2bbd7983646b5b0393f176d362355d6234a
amal <>
2020-08-17 15:44:45 +0100

    interview

--- python3.py
+++ python3.py
@@ -1,1 +1,25 @@
-pyyhon3
+def voisinsCase(Plateau, start, finish):
+	Plateau = [[True, False, False, False], [False, True, True, False]]
+	P1 = [][]
+	for i in Plateau:
+		for j in Plateau
+			p1.append(Plateau[i][j-1])
+			j+=1
+		i+=1
+	return p1
+
+def voisinsCases(Plateau, deb, fin):
+	Plateau = [[True, False, False, False], [False, True, True, False]]
+	P2 = [][]
+	for 
+	return P2
+
+
+def accessibles(Plateau, deb, fin):
+	Plateau = [[True, False, False, False], [False, True, True, False]]
+
+def chemin(Plateau):
+	Plateau = [[True, False, False, False], [False, True, True, False]]
+	My_bool = True
+	if
+	return My_bool
